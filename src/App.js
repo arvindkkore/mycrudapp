be2386d2da80cc3db1f01a0a3281cec72134ac1c
Arvind Kore <arvindkore@cdnsol.com>
2019-12-26 13:37:33 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import ListUserComponent from "./components/ListUserComponent";
+import AddUserComponent from "./components/AddUserComponent";
+import EditUserComponent from "./components/EditUserComponent";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+     <Router> 
+      <nav>
+      </nav> 
+      <Switch>
+        <Route path="/" exact component={ListUserComponent} />
+        <Route path="/users" component={ListUserComponent} />
+        <Route path="/add-user" component={AddUserComponent} />
+        <Route path="/edit-user" component={EditUserComponent} />
+      </Switch>
+     </Router>
     </div>
   );
 }
